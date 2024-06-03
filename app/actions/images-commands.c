d7e1484858aef28ca2d8bd327e50c94a6b69fd21
bootchk <konnekerl@gmail.com>
2024-06-03 16:35:43 -0400

    app:images dockable: disable "Delete" on "New Display" action.
    See #11642, there are more issues.
    Possibly the design should change so that Images dockable behaves like
    images tab bar.

--- app/actions/images-commands.c
+++ app/actions/images-commands.c
@@ -31,10 +31,14 @@
 
 #include "widgets/gimpcontainerview.h"
 #include "widgets/gimpimageview.h"
+#include "widgets/gimpuimanager.h"
+#include "widgets/gimpeditor.h"
+#include "widgets/gimpaction.h"
 
 #include "display/gimpdisplay.h"
 #include "display/gimpdisplayshell.h"
 
+#include "actions/images-actions.h"  /* images-actions-update */
 #include "images-commands.h"
 
 
@@ -90,9 +94,36 @@ images_new_view_cmd_callback (GimpAction *action,
     {
       gimp_create_display (image->gimp, image, GIMP_UNIT_PIXEL, 1.0,
                            G_OBJECT (gimp_widget_get_monitor (GTK_WIDGET (editor))));
+
+      /* Sensitize actions: delete now disabled. */
+      images_actions_update (gimp_action_get_group (action), data);
     }
 }
 
+/* Callback from action "images-delete".
+ *
+ * data is an editor having a ui (menu or button) that user chose to cause action.
+ * The editor is the "Images" dockable.
+ * This is NOT the "view-close" action from: the main menu,
+ * the context popup menu of the main canvas, or a X button in image tab.
+ *
+ * Remove image from the editor's container.
+ * When the image has no displays, dispose of the image.
+ * The image is expected to not have a display.
+ * (The action is disabled when the image does have a display.)
+ * This can happen when an image is created by a plugin or an interpreter console.
+ *
+ * Other widgets that view components of the image can reference the image,
+ * but they must be weak pointers.
+ *
+ * The action name "delete" is somewhat confusing because it is like "close".
+ * It does NOT remove any files.
+ * It only removes an icon of the image from the editor's viewed list,
+ * and removes the image from memory.
+ *
+ * The action "delete" is directly by a user.
+ * The action is NOT a plugin calling API gimp-image-delete.
+ */
 void
 images_delete_image_cmd_callback (GimpAction *action,
                                   GVariant   *value,
@@ -107,10 +138,14 @@ images_delete_image_cmd_callback (GimpAction *action,
   context   = gimp_container_view_get_context (editor->view);
 
   image = gimp_context_get_image (context);
+  g_debug ("%s is_dirty %i", G_STRFUNC, gimp_image_is_dirty (image));
 
   if (image && gimp_container_have (container, GIMP_OBJECT (image)))
     {
+      gimp_container_remove (container, image);
+
       if (gimp_image_get_display_count (image) == 0)
+        /* There is no image_delete.  This will cause image_dispose. */
         g_object_unref (image);
     }
 }
