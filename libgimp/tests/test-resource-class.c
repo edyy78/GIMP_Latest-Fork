e07740dea2f5af23099b67f1c3bfd8887a53bd14
Tejas R <tejas.techstack@gmail.com>
2025-03-15 10:29:28 +0530

    libgimp/tests: move resource-class tests from manual to automated (only python)

---
+++ libgimp/tests/test-resource-class.c
@@ -0,0 +1,14 @@
+static GimpValueArray *
+gimp_c_test_run (GimpProcedure       *procedure,
+                 GimpRunMode          run_mode,
+                 GimpImage           *image,
+                 GimpDrawable       **drawables,
+                 GimpProcedureConfig *config,
+                 gpointer             run_data)
+{
+  GIMP_TEST_START ("Verify resource class.");
+  GIMP_TEST_END (1 == 1);
+
+  GIMP_TEST_RETURN
+}
+
