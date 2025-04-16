97549cde00200c862101d6a05c837b7a3f25785d
programmer_ceds <programmer_ceds@yahoo.co.uk>
2025-04-16 17:49:50 +0100

    Enhance New Guide and New Guide Percent

--- plug-ins/script-fu/scripts/guides-new.scm
+++ plug-ins/script-fu/scripts/guides-new.scm
@@ -8,16 +8,60 @@
                              drawables
                              direction
                              position)
+  (define HORIZONTAL_ONLY                   0)
+  (define VERTICAL_ONLY                     1)
+  (define HORIZONTAL_AND_VERTICAL           2)
+  (define HORIZONTAL_MIRRORED               3)
+  (define VERTICAL_MIRRORED                 4)
+  (define HORIZONTAL_AND_VERTICAL_MIRRORED  5)
+
+  (script-fu-use-v3)
+
   (let* (
-        (width (car (gimp-image-get-width image)))
-        (height (car (gimp-image-get-height image)))
+        (image_max_x         (- (gimp-image-get-width image) 1))
+        (image_max_y         (- (gimp-image-get-height image) 1))
+        (horizontal_reqd     0)
+        (vertical_reqd       0)
+        (mirrored            0)
         )
 
-    (if (= direction 0)
-        ;; check position is inside the image boundaries
-        (if (<= position height) (gimp-image-add-hguide image position))
-        (if (<= position width) (gimp-image-add-vguide image position))
+    (gimp-image-undo-group-start image)
+
+    (if (or (= direction HORIZONTAL_ONLY) (= direction HORIZONTAL_AND_VERTICAL)
+            (= direction HORIZONTAL_MIRRORED) (= direction HORIZONTAL_AND_VERTICAL_MIRRORED))
+        (set! horizontal_reqd 1)
     )
+
+    (if (or (= direction VERTICAL_ONLY) (= direction HORIZONTAL_AND_VERTICAL)
+            (= direction VERTICAL_MIRRORED) (= direction HORIZONTAL_AND_VERTICAL_MIRRORED))
+        (set! vertical_reqd 1)
+    )
+
+    (if (or (= direction HORIZONTAL_MIRRORED) (= direction VERTICAL_MIRRORED)
+            (= direction HORIZONTAL_AND_VERTICAL_MIRRORED))
+        (set! mirrored 1)
+    )
+
+    (if (= horizontal_reqd 1)
+        (gimp-image-add-hguide image position)
+    )
+
+    (if (= vertical_reqd 1)
+         (gimp-image-add-vguide image position)
+    )
+
+    (if (= mirrored 1)
+        (begin
+            (if (and (= horizontal_reqd 1) (not (= position (- image_max_y position))))
+                (gimp-image-add-hguide image (- image_max_y position))
+            )
+            (if (and (= vertical_reqd 1) (not (= position (- image_max_x position))))
+                (gimp-image-add-vguide image (- image_max_x position))
+            )
+        )
+    )
+
+    (gimp-image-undo-group-end image)
 
     (gimp-displays-flush)
   )
@@ -31,8 +75,9 @@
   "2004-04-02"
   "*"
   SF-ONE-OR-MORE-DRAWABLE  ; doesn't matter how many drawables are selected
-  SF-OPTION     _"_Direction" '(_"Horizontal" _"Vertical")
-  SF-ADJUSTMENT _"_Position"  (list 0 0 MAX-IMAGE-SIZE 1 10 0 1)
+  SF-OPTION     _"_Direction" '(_"Horizontal" _"Vertical" _"Horizontal and vertical"
+                _"Horizontal mirrored" _"Vertical mirrored" _"Horizontal and vertical mirrored")
+  SF-ADJUSTMENT _"_Position"  (list 0 (* -1 MAX-IMAGE-SIZE) MAX-IMAGE-SIZE 1 10 0 1)
 )
 
 (script-fu-menu-register "script-fu-guide-new"
